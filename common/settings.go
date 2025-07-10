abe7ccf153fb8c098e2c5b9e0d8f5416ff01a2b5
Barnabas Birmacher <birmacher@gmail.com>
2025-07-10 10:26:17 +0200

    logic tweaks

--- common/settings.go
+++ common/settings.go
@@ -48,42 +48,34 @@ func WithYamlFile() Settings {
 	var filePath string
 	filenames := []string{"review.bitrise.yml", "review.bitrise.yaml"}
 
-	for _, name := range filenames {
-		if _, err := os.Stat(name); err == nil {
-			filePath = name
-			break
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
-	}
 
-	if filePath == "" {
-		filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
+		for _, name := range filenames {
+			if !info.IsDir() && info.Name() == name {
+				filePath = path
+				return filepath.SkipAll
 			}
-			if filePath != "" {
-				return filepath.SkipDir
-			}
-			for _, name := range filenames {
-				if !info.IsDir() && info.Name() == name {
-					filePath = path
-					return filepath.SkipAll
-				}
-			}
-			return nil
-		})
-	}
+		}
+		return nil
+	})
 
-	if filePath != "" {
+	switch filePath {
+	case "":
+		logger.Infof("No YAML file found in the current directory or subdirectories. Using default settings.")
+	default:
+		logger.Infof("Using settings from YAML file: %s", filePath)
 		data, err := os.ReadFile(filePath)
 		if err == nil {
 			if err := yaml.Unmarshal(data, &settings); err != nil {
-				logger.Infof("Failed to parse YAML file %s: %v", filePath, err)
-			} else {
-				logger.Infof("Using settings from YAML file: %s", filePath)
+				logger.Warnf("Failed to parse YAML file %s, switching back to default settings: %v", filePath, err)
 			}
+		} else {
+			logger.Warnf("Failed to read YAML file %s, switching back to default settings: %v", filePath, err)
 		}
-	} else {
-		logger.Infof("No YAML file found in the current directory or subdirectories. Using default settings.")
 	}
+
 	return settings
 }
